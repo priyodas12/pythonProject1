2841e587b0f6968c0c4e7630c8b6f8e9b060b680
Priyobrato.Das <priyodas12@gmail.com>
2023-04-14 22:52:13 +0530

    multiple assignment

---
+++ variableInPython/MultipleAssignment.py
@@ -0,0 +1,13 @@
+# city="BLR"
+# pin=560037
+# temp=32.5
+# isSiliconCity=True
+
+city, pin, temp, isSiliconCity = "KOL", 700001, 43.4, False
+
+print(city, pin, temp, isSiliconCity)
+
+# For  single value assignment
+a = b = c = d = 100
+
+print(a, b, c, d)
