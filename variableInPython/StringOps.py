49453f8067aa5233d734ce82d3a4b9cd953b1547
Priyobrato.Das <priyodas12@gmail.com>
2023-04-14 23:47:25 +0530

    slicing

--- variableInPython/StringOps.py
+++ variableInPython/StringOps.py
@@ -14,3 +14,28 @@ print(he * 3)  # display 3 times he
 print(he.split("r"))  # ['P', 'iyob', 'ato Das']
 print(she.strip())  # Removes white spaces in front and back
 print(he.isprintable())
+
+# Slicing Operation
+
+# [start:stop:step]
+
+f_letter=he[0]
+print(f_letter)
+
+f_name=he[0:10]
+print(f_name)
+
+l_name=he[11:]
+print(l_name)
+
+even_chars=he[0:13:3]
+print(even_chars)
+
+print(he[::3])
+
+reversed_name=he[::-1]
+print(reversed_name)
+
+web1="https://www.g00gle.com"
+
+print("hostname:  "+web1[12:18])
