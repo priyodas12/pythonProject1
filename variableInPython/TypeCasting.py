30d349867127bcd9f70e9a63cbc6bbb77de6d79a
Priyobrato.Das <priyodas12@gmail.com>
2023-04-14 23:15:51 +0530

    type casting

---
+++ variableInPython/TypeCasting.py
@@ -0,0 +1,9 @@
+x, y, z = 100, 100.4, "100"
+
+print(x, y, z)  # 100 100.4 100
+
+print(x, int(y), int(z))  # 100 100 100
+
+print(str(x), str(y), z)  # 100 100.4 100
+
+print(float(x), y , float(z))  # 100.0 100.4 100.0
