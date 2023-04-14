fe16e9a1e1012b64e2a42700713589bec8787ff3
Priyobrato.Das <priyodas12@gmail.com>
2023-04-14 23:34:56 +0530

    basic math ops

---
+++ MathUtility/BasicOps.py
@@ -0,0 +1,12 @@
+import math
+
+pi=3.14
+
+print(round(pi))  # 3
+print(math.ceil(pi))  # 4
+print(math.floor(pi))  # 3
+print(abs(-5.34))  # indicates how far this number from zero.
+print(pow(pi,3))
+print(math.sqrt(9))
+print(max(23,5,32,-3,12))
+print(min(23,5,32,-3,12))
