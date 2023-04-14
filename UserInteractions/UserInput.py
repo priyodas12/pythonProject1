de0a763ea49d7a07952f7d6886d15f13400bd866
Priyobrato.Das <priyodas12@gmail.com>
2023-04-14 23:27:23 +0530

    user interactions

---
+++ UserInteractions/UserInput.py
@@ -0,0 +1,7 @@
+a = input("Please enter your age::")   # Always takes values as string.
+
+print(int(a)+2)
+
+b,c=input("Enter Two different number: ").split(",")
+
+print("Multiplication of two numbers: "+str((int(b)*int(c))))
