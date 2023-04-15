2466bd4642d5b7d2bbffa7075a578018226a2fda
Priyobrato.Das <priyodas12@gmail.com>
2023-04-15 22:01:30 +0530

    while loop

---
+++ ConditionalOps/AgeChecker.py
@@ -0,0 +1,10 @@
+age=input("Please enter your age:: ")
+
+int_age=int(age)
+
+if int_age>=18:
+    print("Adult")
+elif int_age<=5:
+    print("Infant")
+else:
+    print("Yet to go")
---
+++ Loops/WhileLoopOps.py
@@ -0,0 +1,11 @@
+# while loop = a statement that will execute until its condition remains true.
+i = 0
+while i < 10:
+    print("I >> " + str(i))
+    i += 1
+
+name=""
+while name== "":
+    name=input("Enter Your Name: ")
+
+print(name)
